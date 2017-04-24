37a1e57ee8106db9038a1ebfa0ea60e413d6672d
Stefan Johnson <sj@scrimpycat.io>
2017-04-25 03:13:29 +1000

    Add convenient aliases

--- apps/gobstopper_service/mix.exs
+++ apps/gobstopper_service/mix.exs
@@ -12,6 +12,7 @@ defmodule Gobstopper.Service.Mixfile do
             elixir: "~> 1.4",
             build_embedded: Mix.env == :prod,
             start_permanent: Mix.env == :prod,
+            aliases: aliases(),
             deps: deps()
         ]
     end
@@ -46,6 +47,20 @@ defmodule Gobstopper.Service.Mixfile do
             { :comeonin, "~> 3.0" },
             { :guardian, "~> 0.14.2" },
             { :guardian_db, "~> 0.8.0" }
+        ]
+    end
+
+    # Aliases are shortcuts or tasks specific to the current project.
+    # For example, to create, migrate and run the seeds file at once:
+    #
+    #     $ mix ecto.setup
+    #
+    # See the documentation for `Mix` for more info on aliases.
+    defp aliases do
+        [
+            "ecto.setup": ["ecto.create", "ecto.migrate"],
+            "ecto.reset": ["ecto.drop", "ecto.setup"],
+            "test": ["ecto.create --quiet", "ecto.migrate", "test"]
         ]
     end
 end
