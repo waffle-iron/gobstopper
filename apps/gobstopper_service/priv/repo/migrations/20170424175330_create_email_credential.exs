ea672c4c88ce37198cb414f2a82715d23bc8fbe8
Stefan Johnson <sj@scrimpycat.io>
2017-04-25 04:04:48 +1000

    Create email credentials table

---
+++ apps/gobstopper_service/priv/repo/migrations/20170424175330_create_email_credential.exs
@@ -0,0 +1,21 @@
+defmodule Gobstopper.Service.Repo.Migrations.CreateEmailCredential do
+    use Ecto.Migration
+
+    def change do
+        create table(:email_credentials) do
+            add :identity_id, references(:identities),
+                null: false
+
+            add :email, :string,
+                null: false
+
+            add :password_hash, :string,
+                null: false
+
+            timestamps()
+        end
+
+        create index(:email_credentials, [:identity_id], unique: true)
+        create index(:email_credentials, [:email], unique: true)
+    end
+end
