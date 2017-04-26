1ff959f6b962db701c73684b6690247b146ab2ec
Stefan Johnson <sj@scrimpycat.io>
2017-04-26 23:33:05 +1000

    Setup tests

---
+++ apps/gobstopper_service/test/support/case.ex
@@ -0,0 +1,25 @@
+defmodule Gobstopper.Service.Case do
+    @moduledoc """
+      This module defines the test case to be used by all tests.
+    """
+
+    use ExUnit.CaseTemplate
+
+    using do
+        quote do
+            import Ecto
+            import Ecto.Changeset
+            import Ecto.Query
+        end
+    end
+
+    setup tags do
+        :ok = Ecto.Adapters.SQL.Sandbox.checkout(Gobstopper.Service.Repo)
+
+        unless tags[:async] do
+            Ecto.Adapters.SQL.Sandbox.mode(Gobstopper.Service.Repo, { :shared, self() })
+        end
+
+        :ok
+    end
+end
