ae92c61c8f483b870e1562f862d4276ecacb473c
Stefan Johnson <sj@scrimpycat.io>
2017-04-26 16:19:39 +1000

    Tokenize identity resource

--- apps/gobstopper_service/lib/gobstopper.service/guardian_serializer.ex
+++ apps/gobstopper_service/lib/gobstopper.service/guardian_serializer.ex
@@ -1,7 +1,9 @@
 defmodule Gobstopper.Service.GuardianSerializer do
     @behaviour Guardian.Serializer
 
+    def for_token(identity = %Gobstopper.Service.Auth.Identity.Model{}), do: { :ok, "Identity:#{identity.id}" }
     def for_token(_), do: { :error, "Unknown resource type" }
 
+    def from_token("Identity:" <> id), do: { :ok, Gobstopper.Service.Repo.get(Gobstopper.Service.Auth.Identity.Model, id) }
     def from_token(_), do: { :error, "Unknown resource type" }
 end
