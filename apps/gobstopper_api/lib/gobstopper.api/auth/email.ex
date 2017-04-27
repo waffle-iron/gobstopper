af1cbf2121b568166da6ed8a3ce10759aa3b7b1b
Stefan Johnson <sj@scrimpycat.io>
2017-04-28 02:17:56 +1000

    Expose email credential api

---
+++ apps/gobstopper_api/lib/gobstopper.api/auth/email.ex
@@ -0,0 +1,68 @@
+defmodule Gobstopper.API.Auth.Email do
+    @moduledoc """
+      Handles the management of email authorization credentials.
+    """
+
+    @service Gobstopper.Service.Auth
+    @credential_type :email
+
+    @type token :: String.t
+
+    @doc """
+      Create a new identity initially associated with the given email credential.
+
+      Returns the session token on successful creation. Otherwise returns an
+      error.
+    """
+    @spec register(String.t, String.t) :: { :ok, token } | { :error, String.t }
+    def register(email, pass), do: GenServer.call(@service, { :create, { @credential_type, { email, pass } } })
+
+    @doc """
+      Get the current email credential associated with the identity.
+
+      Returns the state of the credential if one exists or does not exist. Otherwise
+      returns an error.
+    """
+    @spec get(token) :: { :ok, { :unverified | :verified, String.t } | { :none, nil } } | { :error, String.t }
+    def get(token) do
+        case GenServer.call(@service, { :all_credentials, token }) do
+            { :ok, credentials } -> credentials[@credential_type]
+            error -> error
+        end
+    end
+
+    @doc """
+      Associate an email credential with the identity, replacing the old email
+      credential.
+
+      Returns `:ok` on successful creation. Otherwise returns an error.
+    """
+    @spec set(token, String.t, String.t) :: :ok | { :error, String.t }
+    def set(token, email, pass) do
+        credential = { @credential_type, { email, pass } }
+
+        with { :error, _update_error } <- GenServer.call(@service, { :update, credential, token }),
+             { :error, create_error } <- GenServer.call(@service, { :create, credential, token }) do
+                { :error, create_error }
+        else
+            :ok -> :ok
+        end
+    end
+
+    @doc """
+      Remove the email credential associated with the identity.
+
+      Returns `:ok` on successful removal. Otherwise returns an error.
+    """
+    @spec remove(token) :: :ok | { :error, String.t }
+    def remove(token), do: GenServer.call(@service, { :remove, { @credential_type }, token })
+
+    @doc """
+      Check if an email credential is associated with the identity.
+
+      Returns whether the credential exists or not, if successful. Otherwise returns
+      an error.
+    """
+    @spec exists?(token) :: { :ok, boolean } | { :error, String.t }
+    def exists?(token), do: GenServer.call(@service, { :credential?, { @credential_type }, token })
+end
