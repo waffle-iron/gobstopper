db02a573f56218779eaddc4f6771777a3b344911
Stefan Johnson <sj@scrimpycat.io>
2017-04-29 00:20:56 +1000

    Start service and setup database

--- apps/gobstopper_api/test/test_helper.exs
+++ apps/gobstopper_api/test/test_helper.exs
@@ -1,1 +1,8 @@
 ExUnit.start()
+
+Gobstopper.Service.Repo.__adapter__.storage_up(Gobstopper.Service.Repo.config)
+
+Gobstopper.Service.start(:normal, [])
+
+migrations = Application.app_dir(:gobstopper_service, "priv/repo/migrations")
+Ecto.Migrator.run(Gobstopper.Service.Repo, migrations, :up, all: true)
