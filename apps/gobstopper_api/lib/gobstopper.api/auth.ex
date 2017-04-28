f11e4d7970bed55603673eb08ed0bfbb9daefe33
Stefan Johnson <sj@scrimpycat.io>
2017-04-28 10:02:57 +1000

    Expose auth api

---
+++ apps/gobstopper_api/lib/gobstopper.api/auth.ex
@@ -0,0 +1,27 @@
+defmodule Gobstopper.API.Auth do
+    @moduledoc """
+      Handles the authorization of session tokens.
+    """
+
+    @service Gobstopper.Service.Auth
+
+    @type token :: String.t
+
+    @doc """
+      Logout of an identity's active session.
+    """
+    @spec logout(token) :: :ok
+    def logout(token) do
+        GenServer.cast(@service, { :logout, token })
+        :ok
+    end
+
+    @doc """
+      Verify an identity's session.
+
+      Returns the unique ID of the identity if verifying a valid session token.
+      Otherwise returns `nil`.
+    """
+    @spec verify(token) :: integer | nil
+    def verify(token), do: GenServer.call(@service, { :verify, token })
+end
