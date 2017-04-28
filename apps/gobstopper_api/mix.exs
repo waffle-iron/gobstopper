74cda987a2f949b6b2443c58f96c9f8e6bbc5602
Stefan Johnson <sj@scrimpycat.io>
2017-04-29 00:20:33 +1000

    Include service case template

--- apps/gobstopper_api/mix.exs
+++ apps/gobstopper_api/mix.exs
@@ -10,6 +10,7 @@ defmodule Gobstopper.API.Mixfile do
             deps_path: "../../deps",
             lockfile: "../../mix.lock",
             elixir: "~> 1.4",
+            elixirc_paths: elixirc_paths(Mix.env),
             build_embedded: Mix.env == :prod,
             start_permanent: Mix.env == :prod,
             deps: deps(Mix.Project.umbrella?)
@@ -22,6 +23,10 @@ defmodule Gobstopper.API.Mixfile do
     def application do
         [extra_applications: [:logger]]
     end
+
+    # Specifies which paths to compile per environment.
+    defp elixirc_paths(:test), do: ["lib", "test/support", "../gobstopper_service/test/support"]
+    defp elixirc_paths(_),     do: ["lib"]
 
     # Dependencies can be Hex packages:
     #
