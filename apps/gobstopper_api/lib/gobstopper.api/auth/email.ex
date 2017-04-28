88468dc619045ad75853bc81390a98c45a8d7f5a
Stefan Johnson <sj@scrimpycat.io>
2017-04-28 10:03:18 +1000

    Use Auth's token type

--- apps/gobstopper_api/lib/gobstopper.api/auth/email.ex
+++ apps/gobstopper_api/lib/gobstopper.api/auth/email.ex
@@ -6,7 +6,7 @@ defmodule Gobstopper.API.Auth.Email do
     @service Gobstopper.Service.Auth
     @credential_type :email
 
-    @type token :: String.t
+    alias Gobstopper.API.Auth
 
     @doc """
       Create a new identity initially associated with the given email credential.
@@ -14,7 +14,7 @@ defmodule Gobstopper.API.Auth.Email do
       Returns the session token on successful creation. Otherwise returns an
       error.
     """
-    @spec register(String.t, String.t) :: { :ok, token } | { :error, String.t }
+    @spec register(String.t, String.t) :: { :ok, Auth.token } | { :error, String.t }
     def register(email, pass), do: GenServer.call(@service, { :create, { @credential_type, { email, pass } } })
 
     @doc """
@@ -23,7 +23,7 @@ defmodule Gobstopper.API.Auth.Email do
       Returns the state of the credential if one exists or does not exist. Otherwise
       returns an error.
     """
-    @spec get(token) :: { :ok, { :unverified | :verified, String.t } | { :none, nil } } | { :error, String.t }
+    @spec get(Auth.token) :: { :ok, { :unverified | :verified, String.t } | { :none, nil } } | { :error, String.t }
     def get(token) do
         case GenServer.call(@service, { :all_credentials, token }) do
             { :ok, credentials } -> credentials[@credential_type]
@@ -37,7 +37,7 @@ defmodule Gobstopper.API.Auth.Email do
 
       Returns `:ok` on successful creation. Otherwise returns an error.
     """
-    @spec set(token, String.t, String.t) :: :ok | { :error, String.t }
+    @spec set(Auth.token, String.t, String.t) :: :ok | { :error, String.t }
     def set(token, email, pass) do
         credential = { @credential_type, { email, pass } }
 
@@ -54,7 +54,7 @@ defmodule Gobstopper.API.Auth.Email do
 
       Returns `:ok` on successful removal. Otherwise returns an error.
     """
-    @spec remove(token) :: :ok | { :error, String.t }
+    @spec remove(Auth.token) :: :ok | { :error, String.t }
     def remove(token), do: GenServer.call(@service, { :remove, { @credential_type }, token })
 
     @doc """
@@ -63,7 +63,7 @@ defmodule Gobstopper.API.Auth.Email do
       Returns whether the credential exists or not, if successful. Otherwise returns
       an error.
     """
-    @spec exists?(token) :: { :ok, boolean } | { :error, String.t }
+    @spec exists?(Auth.token) :: { :ok, boolean } | { :error, String.t }
     def exists?(token), do: GenServer.call(@service, { :credential?, { @credential_type }, token })
 
     @doc """
@@ -71,6 +71,6 @@ defmodule Gobstopper.API.Auth.Email do
 
       Returns the session token on successful login. Otherwise returns an error.
     """
-    @spec login(String.t, String.t) :: { :ok, token } | { :error, String.t }
+    @spec login(String.t, String.t) :: { :ok, Auth.token } | { :error, String.t }
     def login(email, pass), do: GenServer.call(@service, { :login, { @credential_type, { email, pass } } })
 end
