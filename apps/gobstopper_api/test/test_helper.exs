3b0bddcd3d547ad41d7b2c62d41e9c846d03dd3a
Stefan Johnson <sj@scrimpycat.io>
2017-04-29 21:01:26 +1000

    Fix up applications required for test

--- apps/gobstopper_api/test/test_helper.exs
+++ apps/gobstopper_api/test/test_helper.exs
@@ -1,8 +1,11 @@
-ExUnit.start()
-
 Gobstopper.Service.Repo.__adapter__.storage_up(Gobstopper.Service.Repo.config)
 
-Gobstopper.Service.start(:normal, [])
+Application.ensure_all_started(:gobstopper_service)
+Application.ensure_all_started(:ecto)
 
 migrations = Application.app_dir(:gobstopper_service, "priv/repo/migrations")
 Ecto.Migrator.run(Gobstopper.Service.Repo, migrations, :up, all: true)
+
+ExUnit.start()
+
+Ecto.Adapters.SQL.Sandbox.mode(Gobstopper.Service.Repo, :manual)
