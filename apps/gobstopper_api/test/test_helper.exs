fca060f2bf173532eba0a2f9187b7fc81d626dc5
Stefan Johnson <sj@scrimpycat.io>
2017-04-29 23:44:23 +1000

    For now just test from umbrella project

--- apps/gobstopper_api/test/test_helper.exs
+++ apps/gobstopper_api/test/test_helper.exs
@@ -1,10 +1,11 @@
-Gobstopper.Service.Repo.__adapter__.storage_up(Gobstopper.Service.Repo.config)
+#For now run test from the umbrella project root
+# Gobstopper.Service.Repo.__adapter__.storage_up(Gobstopper.Service.Repo.config)
 
 Application.ensure_all_started(:gobstopper_service)
 Application.ensure_all_started(:ecto)
 
-migrations = Application.app_dir(:gobstopper_service, "priv/repo/migrations")
-Ecto.Migrator.run(Gobstopper.Service.Repo, migrations, :up, all: true)
+# migrations = Application.app_dir(:gobstopper_service, "priv/repo/migrations")
+# Ecto.Migrator.run(Gobstopper.Service.Repo, migrations, :up, all: true)
 
 ExUnit.start()
 
