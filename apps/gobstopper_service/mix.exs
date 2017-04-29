c1e41b6490febf5fa8aa37b3f7a7a75a9e86d65b
Stefan Johnson <sj@scrimpycat.io>
2017-04-30 01:38:48 +1000

    Drop database on test

--- apps/gobstopper_service/mix.exs
+++ apps/gobstopper_service/mix.exs
@@ -68,7 +68,7 @@ defmodule Gobstopper.Service.Mixfile do
         [
             "ecto.setup": ["ecto.create", "ecto.migrate"],
             "ecto.reset": ["ecto.drop", "ecto.setup"],
-            "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+            "test": ["ecto.drop --quiet", "ecto.create --quiet", "ecto.migrate", "test"]
         ]
     end
 end
